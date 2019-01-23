a02d495c6e8d49fe99c1ffd4ecd8884aa1d60981
Tom Shiflet <tom.shiflet@mylcs.com>
2019-01-23 10:49:19 -0600

    Initial Commit

---
+++ TileMapTest/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  TileMapTest
+//
+//  Created by Tom Shiflet on 1/23/19.
+//  Copyright © 2019 Tom Shiflet. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
